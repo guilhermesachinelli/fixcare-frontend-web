b1271dbd30ec54ea27a9783daf710556c555ef59
Isabelle Barquilia <isabelle-moraes@aluno.senai>
2024-09-16 17:04:51 -0300

    feat? page doc geral

--- app/DocGeral/page.jsx
+++ app/DocGeral/page.jsx
@@ -5,19 +5,23 @@ function page() {
         <div className={styles.page}>
             {/* Bloco das informções do manuel mecânico */}
             <main className={styles.card}>
-                <h1>Manual Mecânico</h1>
+                <img src="/engrenagem.png" className={styles.img} />
+                <h1 className={styles.title}>Manual Mecânico</h1>
             </main>
             {/* Bloco das informções do manuel elétrico */}
             <main className={styles.card}>
-                <h1>Manual Elétrico</h1>
+            <img src="/eletrico.png" className={styles.img2} />
+                <h1 className={styles.title}>Manual Elétrico</h1>
             </main>
             {/* Bloco das informções da ficha de dados de segurança */}
             <main className={styles.card}>
-                <h1>Ficha de Dados de Segurança</h1>
+            <img src="/fds.png" className={styles.img2} />
+                <h1 className={styles.title}>Ficha de Dados de Segurança</h1>
             </main>
             {/* Bloco das informções do protocolo de segurança */}
             <main className={styles.card}>
-                <h1>Protocolo de Segurança</h1>
+            <img src="/seguranca.png" className={styles.img3} />
+                <h1 className={styles.title}>Protocolo de Segurança</h1>
             </main>
         </div>
         
