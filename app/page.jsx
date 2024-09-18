fdd3b2a0c4ff37d4aa2e1479886a3870e6e46f20
richardmsiqueira <richard.siqueira@aluno.senai.br>
2024-09-18 11:27:05 -0300

    header e footer prontos

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,20 @@
 import styles from "./page.module.css";
 import Header from "./components/header/page.jsx";
+import Footer from "./components/footer/page.jsx";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-      <Header />
+        <Header />
       </header>
       <div className={styles.Imgs}>
         <img className={styles.logoSenai} src="/senaiLogo.png" alt="Banner" />
         <img className={styles.tornoImg} src="/tornoHome.png" alt="Banner" />
       </div>
-      
+      <footer className={styles.footer}>
+        <Footer />
+      </footer>
     </div>
   );
 }
