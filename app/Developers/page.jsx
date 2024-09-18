f50d0103e8a58464b653505c6f3f7fea5df45501
GuiHJLima <guilhermehenriquelima.564@gmail.com>
2024-09-18 11:18:35 -0300

    developers and scroll bar

--- app/Developers/page.jsx
+++ app/Developers/page.jsx
@@ -1,41 +1,45 @@
 import Footer from "../components/footer/page";
+import Header from "../components/header/page";
 import styles from "./page.module.css"
 
 function page() {
     return (
         <div className={styles.container}>
+            <header className={styles.header}>
+                <Header />
+            </header>
             <div className={styles.Cards}>
-                <a href='./ManualMecanico'> 
-                <div className={styles.manualM}>
-                    <img className={styles.engrenagens} src="/engrenagens.png" />
-                    <h1 className={styles.titulo}>Guilherme Lima</h1>
-                </div>
+                <a href='./ManualMecanico'>
+                    <div className={styles.manualM}>
+                        <img className={styles.engrenagens} src="/engrenagens.png" />
+                        <h1 className={styles.titulo}>Guilherme Lima</h1>
+                    </div>
                 </a>
                 <a href='./ManualEletrico'>
-                <div className={styles.manualE}>
-                    <img className={styles.eletrica} src="/eletrico.png" />
-                    <h1 className={styles.titulo}>Richard Macedo</h1>
-                </div>
+                    <div className={styles.manualE}>
+                        <img className={styles.eletrica} src="/eletrico.png" />
+                        <h1 className={styles.titulo}>Richard Macedo</h1>
+                    </div>
                 </a>
                 <a href='./FDS'>
-                <div className={styles.Fds}>
-                    <img className={styles.fds} src="/fds.png" />
-                    <h1 className={styles.titulo}>Guilherme Godoy</h1>
-                </div>
+                    <div className={styles.Fds}>
+                        <img className={styles.fds} src="/fds.png" />
+                        <h1 className={styles.titulo}>Guilherme Godoy</h1>
+                    </div>
                 </a>
             </div>
             <div className={styles.Cards}>
-                <a href='./ManualMecanico'> 
-                <div className={styles.manualM}>
-                    <img className={styles.engrenagens} src="/engrenagens.png" />
-                    <h1 className={styles.titulo}>Luana Fascini</h1>
-                </div>
+                <a href='./ManualMecanico'>
+                    <div className={styles.manualM}>
+                        <img className={styles.engrenagens} src="/engrenagens.png" />
+                        <h1 className={styles.titulo}>Luana Fascini</h1>
+                    </div>
                 </a>
                 <a href='./ManualEletrico'>
-                <div className={styles.manualE}>
-                    <img className={styles.eletrica} src="/eletrico.png" />
-                    <h1 className={styles.titulo}>Isabele Barquilia</h1>
-                </div>
+                    <div className={styles.manualE}>
+                        <img className={styles.eletrica} src="/eletrico.png" />
+                        <h1 className={styles.titulo}>Isabele Barquilia</h1>
+                    </div>
                 </a>
             </div>
             <footer className={styles.footer}>
