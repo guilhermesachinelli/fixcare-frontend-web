163f992450a56c41eb1f9d2e5d5aeaa255b2daed
Isabelle Barquilia <isabelle-moraes@aluno.senai.sp>
2024-09-23 11:53:10 -0300

    feat: sem tempo

--- app/FDS/page.jsx
+++ app/FDS/page.jsx
@@ -15,33 +15,34 @@ function Fds() {
 
             <div className={styles.Card}>
                 <div className={styles.text1}>
-                <h1 className={styles.titulos}>
-                O que é a FDS?
-                </h1>
-                <p className={styles.textos}>
-                É um documento que contém informações detalhadas sobre um produto químico, como seus ingredientes, riscos à saúde e ao meio ambiente, instruções de uso seguro e procedimentos em caso de emergência.
-                </p>
+                    <h1 className={styles.titulos}>
+                        O que é a FDS?
+                    </h1>
+                    <p className={styles.textos}>
+                        É um documento que contém informações detalhadas sobre um produto químico, como seus ingredientes, riscos à saúde e ao meio ambiente, instruções de uso seguro e procedimentos em caso de emergência.
+                    </p>
                 </div>
 
                 <div className={styles.text2}>
-                <h1 className={styles.titulos}>
-                Por que a FDS é importante? 
-                </h1>
-                <p className={styles.textos}>
-                Ela é como um "manual de instruções" do produto, garantindo que o usuário saiba como manuseá-lo de forma segura e evitar acidentes.
-                </p>
+                    <h1 className={styles.titulos}>
+                        Por que a FDS é importante?
+                    </h1>
+                    <p className={styles.textos}>
+                        Ela é como um "manual de instruções" do produto, garantindo que o usuário saiba como manuseá-lo de forma segura e evitar acidentes.
+                    </p>
                 </div>
 
                 <div className={styles.text3}>
                     <p className={styles.textos}>
-                    FDS é um documento essencial para quem trabalha ou utiliza produtos químicos, pois fornece todas as informações necessárias para garantir a segurança e o bom uso desses produtos.
+                        FDS é um documento essencial para quem trabalha ou utiliza produtos químicos, pois fornece todas as informações necessárias para garantir a segurança e o bom uso desses produtos.
                     </p>
                 </div>
 
-                <div className={styles.informacao}>
-                    <img src="/maisInformacao.png" alt="FDS" className={styles.imagem}/>
-                    <p className={styles.informacao}>Mais Informações</p>
-                    </div>
+            </div>
+
+            <div className={styles.informacao}>
+                <img src="/maisInformacao.png" alt="FDS" className={styles.imagem} />
+                <p className={styles.informacao}>Mais Informações</p>
             </div>
 
             <footer className={styles.footer}>
