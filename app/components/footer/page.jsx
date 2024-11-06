c87c416f6383cd9e93a0487a1693c093295b0c0d
Guilherme Augusto De Godoy Sachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2024-11-06 11:54:41 -0300

    nova home em andamento

--- app/components/footer/page.jsx
+++ app/components/footer/page.jsx
@@ -1,17 +1,64 @@
 import React from 'react';
 import styles from './page.module.css';
-import { FaInstagram, FaGithub } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <div className={styles.footerContent}>
-
-      <div className={styles.footerLogo}>
-        <img src="/fixcare.png" alt="Logo da fixcare" width={80} height={80}/>
+    <div className={styles.footer}>
+      <div className={styles.sbFooterSectionPadding}>
+        <div className={styles.sbFooterLinks}>
+          <div className={styles.sbFooterLinksDiv}>
+            <h4>Páginas</h4>
+            <a href="/">
+              <p>Home</p>
+            </a>
+            <a href="./DocGeral">
+              <p>Documentação</p>
+            </a>
+            <a href="./LoginServicos">
+              <p>Login</p>
+            </a>
+            <a href="./Adm">
+              <p>Adiministração</p>
+            </a>
+          </div>
+          <div className={styles.sbFooterLinksDiv}>
+            <h4>Documentos</h4>
+            <a href="./FDS">
+              <p>FDS</p>
+            </a>
+            <a href="./ProtocoloSeguranca">
+              <p>Home</p>
+            </a>
+          </div>
+          <div className={styles.sbFooterLinksDiv}>
+            <h4>Desenvolvedores</h4>
+            <a href="./Developers">
+              <p>Nossa Equipe</p>
+            </a>
+          </div>
+          <div className={styles.sbFooterLinksDiv}>
+            <h4>Coming soon on</h4>
+            <div className={styles.socialMidia}>
+              
+            </div>
+          </div>
         </div>
 
-      <div className={styles.footerText}>
-        <p>© 2024 - Todos os direitos reservados AllCode.</p>
+      <hr className={styles.hr}></hr>
+
+      <div className={styles.sbFooterBelow}>
+        <div className={styles.sbFooterCopyright}>
+          <p>
+          ©{new Date().getFullYear()} AllCode - Todos os direitos reservados.
+          </p>
+        </div>
+        <div className={styles.sbFooterBelowLinks}>
+          <div><p>Termos & Condições</p></div>
+          <div><p>Privacidade</p></div>
+          <div><p>Segurança</p></div>
+        </div>
+      </div>
+
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,22 @@
-// Indica que este componente deve ser renderizado no lado do cliente.
 'use client';
 
-// Importa os estilos definidos no arquivo CSS.
 import styles from "./page.module.css";
-
-// Importa o componente Header.
 import Header from "./components/header/page.jsx";
-
-// Importa o componente Footer.
 import Footer from "./components/footer/page.jsx";
+import Body from "./components/Body/page";
 
-// Define o componente funcional Home.
 const Home = () => {
   return (
-    // Aplica a classe CSS 'container' ao div principal.
     <div className={styles.container}>
 
-      {/* Define o cabeçalho da página e aplica a classe CSS 'header'.*/}
-        {/*Renderiza o componente Header.*/}
+      
         <Header />
-      {/* Define uma div para conter as imagens e aplica a classe CSS 'Imgs'.*/}
-      <div className={styles.Imgs}>
-        {/* Renderiza a imagem do logo do Senai e aplica a classe CSS 'logoSenai'.*/}
-        <img className={styles.logoSenai} src="/senaibluelogos.png" alt="Banner" />
-        {/* Renderiza a imagem do torno e aplica a classe CSS 'tornoImg'.*/}
-        <img className={styles.tornoImg} src="/tornoHome.png" alt="Banner" />
-      </div>
-      {/* Define o rodapé da página e aplica a classe CSS 'footer'.*/}
-      <footer className={styles.footer}>
-        {/*Renderiza o componente Footer.*/}
+      <Body />
         <Footer />
-      </footer>
+      
     </div>
   );
 }
 
-// Exporta o componente Home como padrão.
+
 export default Home; 
