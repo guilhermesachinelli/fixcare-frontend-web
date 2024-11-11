d77998e87745cc72c0a2e7e1b542739ddf7a7f5e
Richard <richard.siqueira@aluno.senai.br>
2024-11-11 14:57:43 -0300

    icon menu retirado

--- app/components/Logo/page.jsx
+++ app/components/Logo/page.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from './page.module.css';
-import { DatabaseOutlined } from '@ant-design/icons';
 
 const Logo = () => {
     return (
         <div className={styles.logo}> 
             <div className={styles.logoicon}>
-                <DatabaseOutlined />
+                
             </div>
         </div>
     )
