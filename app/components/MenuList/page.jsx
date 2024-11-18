ca89d2edcaa85f05b1843eccf386256d221eb44a
LuFassini <luanafassini.564@gmail.com>
2024-11-18 11:24:16 -0300

    feat: correcao menu

--- app/components/MenuList/page.jsx
+++ app/components/MenuList/page.jsx
@@ -1,58 +1,33 @@
-
-import {
-    HomeOutlined,
-    LoginOutlined,
-    SettingOutlined,
-    ContainerOutlined,
-    TeamOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
+import React from 'react';
+import { Menu } from 'antd';
+import { HomeOutlined, UserOutlined, TeamOutlined } from '@ant-design/icons';
 import styles from './page.module.css';
 
 const MenuList = ({ darkTheme }) => {
-    return (
-        <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className={styles.menubar}>
-            <Menu.Item key="home" icon={<HomeOutlined />}>
-                <a href='./'>
-                    Home
-                </a>
-            </Menu.Item>
-
-            <Menu.SubMenu
-                key="subtasks"
-                icon={<ContainerOutlined />}
-                title="Documentação"
-            >
-                <a href='./FDS'>
-                    <Menu.Item key="task-1">FDS</Menu.Item>
-                </a>
-                <a href='./ProtocoloSeguranca'>
-                    <Menu.Item key="task-2">Protocolo de Segurança</Menu.Item>
-                </a>
-            </Menu.SubMenu>
-
-            <Menu.Item key="machine" icon={<SettingOutlined />}>
-                <a href='./Maquinas'>
-                    Máquinas
-                </a>
-            </Menu.Item>
-            <Menu.Item key="login" icon={<LoginOutlined />}>
-                <a href='./LoginServicos'>
-                    Login
-                </a>
-            </Menu.Item>
-            <Menu.Item key="adm" icon={<UserOutlined />}>
-                <a href='./Adm'>
-                    Administração
-                </a>
-            </Menu.Item>
-            <Menu.Item key="team" icon={<TeamOutlined />}>
-                <a href='./Developers'>
-                    Desenvolvedores
-                </a>
-            </Menu.Item>
-        </Menu>
-    )
-}
+  return (
+    <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className={styles.menubar}>
+      <Menu.Item key="home" icon={<HomeOutlined />}>
+        <a href='./'>
+          Home
+        </a>
+      </Menu.Item>
+      <Menu.Item key="login" icon={<UserOutlined />}>
+        <a href='./LoginServicos'>
+          Login
+        </a>
+      </Menu.Item>
+      <Menu.Item key="adm" icon={<UserOutlined />}>
+        <a href='./Adm'>
+          Administração
+        </a>
+      </Menu.Item>
+      <Menu.Item key="team" icon={<TeamOutlined />}>
+        <a href='./Developers'>
+          Desenvolvedores
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 export default MenuList;
