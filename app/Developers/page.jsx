bd0d1800f2a045f5a1287c2a226ca1c0a1d82bc2
Luana Bergamo Fassini <luanafassini.564@gmail.com>
2024-11-18 15:22:22 -0300

    feat: ajustes desenvolvedores

--- app/Developers/page.jsx
+++ app/Developers/page.jsx
@@ -1,57 +1,56 @@
 import Footer from "../components/footer/page";
-import SideBar from "../components/SideBar/page";
+import SideBar from "../components/sidebar/page"; // Corrigido para "sidebar"
 
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 
-function page() {
+function Page() { // Corrigido para "Page" com letra maiúscula
     return (
         <div className={styles.container}>
-
             <SideBar />
-
-            <div className={styles.titleContainer}>
-                <h1 className={styles.titulo1}>Desenvolvedores</h1>
-            </div>
-
-            <div className={styles.Cards}>
-                <a href='https://github.com/GuiHJLima'>
-                    <div className={styles.manualM}>
-                        <img className={styles.engrenagens} src="/lima.jpeg" />
-                        <h1 className={styles.nomes}>Guilherme Lima</h1>
-                    </div>
-                </a>
-                <a href='https://github.com/richardmsiqueira'>
-                    <div className={styles.manualE}>
-                        <img className={styles.engrenagens} src="/richard.jpeg" />
-                        <h1 className={styles.nomes}>Richard Macedo</h1>
-                    </div>
-                </a>
-                <a href='https://github.com/IsaBarquilia'>
-                    <div className={styles.Fds}>
-                        <img className={styles.engrenagens} src="/isa.jpeg" />
-                        <h1 className={styles.nomes}>Isabelle Barquilia</h1>
-                    </div>
-                </a>
-            </div>
-            <div className={styles.Cards}>
-                <a href='https://github.com/LuFassini'>
-                    <div className={styles.manualM}>
-                        <img className={styles.engrenagens} src="/luana.jpeg" />
-                        <h1 className={styles.nomes}>Luana Fassini</h1>
-                    </div>
-                </a>
-                <a href='https://github.com/guilhermesachinelli'>
-                    <div className={styles.manualE}>
-                        <img className={styles.engrenagens} src="/godoy.jpg" />
-                        <h1 className={styles.nomes}>Guilherme Godoy</h1>
-                    </div>
-                </a>
+            <div className={styles.allcards}>
+                <div className={styles.titleContainer}>
+                    <h1 className={styles.titulo1}>Desenvolvedores</h1>
+                </div>
+                <div className={styles.Cards1}>
+                    <a href='https://github.com/GuiHJLima'>
+                        <div className={styles.individual}>
+                            <img className={styles.foto} src="/lima.jpeg" alt="Guilherme Lima" />
+                            <h1 className={styles.nomes}>Guilherme Lima</h1>
+                        </div>
+                    </a>
+                    <a href='https://github.com/richardmsiqueira'>
+                        <div className={styles.individual}>
+                            <img className={styles.foto} src="/richard.jpeg" alt="Richard Macedo" />
+                            <h1 className={styles.nomes}>Richard Macedo</h1>
+                        </div>
+                    </a>
+                    <a href='https://github.com/IsaBarquilia'>
+                        <div className={styles.individual}>
+                            <img className={styles.foto} src="/isa.jpeg" alt="Isabelle Barquilia" />
+                            <h1 className={styles.nomes}>Isabelle Barquilia</h1>
+                        </div>
+                    </a>
+                </div>
+                <div className={styles.Cards2}>
+                    <a href='https://github.com/LuFassini'>
+                        <div className={styles.individual}>
+                            <img className={styles.foto} src="/luana.jpeg" alt="Luana Fassini" />
+                            <h1 className={styles.nomes}>Luana Fassini</h1>
+                        </div>
+                    </a>
+                    <a href='https://github.com/guilhermesachinelli'>
+                        <div className={styles.individual}>
+                            <img className={styles.foto} src="/godoy.jpg" alt="Guilherme Godoy" />
+                            <h1 className={styles.nomes}>Guilherme Godoy</h1>
+                        </div>
+                    </a>
+                </div>
             </div>
             <footer className={styles.footer}>
                 <Footer />
             </footer>
         </div>
-    )
+    );
 }
 
-export default page;
+export default Page;
